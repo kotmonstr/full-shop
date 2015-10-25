40594dca4b5e5cb71986e3848a278c42575acdb6
kotmonstr <kotmonstr@ukr.net>
2015-10-25 19:22:43 +0300

    phpshtorm

---
+++ common/models/OrderStatus.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order_status".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $descr
+ *
+ * @property Order[] $orders
+ */
+class OrderStatus extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'order_status';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['descr'], 'integer'],
+            [['name'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'descr' => 'Descr',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrders()
+    {
+        return $this->hasMany(Order::className(), ['status' => 'id']);
+    }
+}
